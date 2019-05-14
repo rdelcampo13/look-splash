b7f4f4ef5df8c83b77ca75a75bc1b3790c7baa33
Rudy Del Campo <r.delcampo13@gmail.com>
2019-05-13 23:52:46 -0500

    Remove lower section of splash page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -179,145 +179,15 @@ class App extends Component {
     return (
       <div>
 
-        <div className={this.state.navClass}>
-          <nav className="navbar navbar-expand-lg navbar-dark fixed-top mobile-nav" style={{ backgroundColor: "#000000" }}>
-              {/* <div className="container"> */}
 
-                <a className="navbar-brand" >
-                  <img src="/img/look-box-logo.png" width="auto" height="30" alt="" />
-                </a>
-
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                  <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                      <a className="nav-link" target="_blank" href="https://vimeo.com/lookscience">Demo</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="/docs/Look_Whitepaper_v1.1.pdf" target="_Blank">Whitepaper</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#" data-toggle="modal" data-target="#mc-modal" >Contact</a>
-                    </li>
-                  </ul>
-                </div>
-              {/* </div> */}
-            </nav>
-        </div>
-
-        <div className="header">
+        <div className="header" style={{ marginBottom: "0" }}>
 
           <Canvas />
 
-          <div className="header-container text-center">
-            {/* <h3 className="header-subtitle header-mobile-title">an augmented scientist approaches...</h3> */}
-            {/* <h1 className="header-title">look.<span className="header-title-ar">ar</span></h1> */}
-            {/* <!-- Button trigger modal --> */}
-            {/* <button type="button" className="btn btn-look" data-toggle="modal" data-target="#mc-modal">
-              Sign up for updates
-            </button> */}
-          </div>
         </div>
 
         <h3 className="header-subtitle header-corner text-center  ">an augmented scientist approaches...</h3>
         <a className="ca3-scroll-down-link ca3-scroll-down-arrow" data-ca3_iconfont="ETmodules" data-ca3_icon=""></a>
-
-        <div className="fullscreen">
-            <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: "#000000" }}>
-              {/* <div className="container"> */}
-
-                <a className="navbar-brand" >
-                  <img src="/img/look-box-logo.png" width="auto" height="30" alt="" />
-                </a>
-
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                  <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                      <a className="nav-link" target="_blank" href="https://vimeo.com/lookscience">Demo</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="/docs/Look_Whitepaper_v1.1.pdf" target="_Blank">Whitepaper</a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" href="#" data-toggle="modal" data-target="#mc-modal" >Contact</a>
-                    </li>
-                  </ul>
-                </div>
-              {/* </div> */}
-            </nav>
-
-          <div className="demo">
-            <div className="container-fluid demo-cont">
-
-              <div className="content-container text-center ar-header">
-                <h2 style={{ color: "#fff", fontFamily: "Montserrat" }}>augmented reality</h2>
-                <h2 style={{ color: "#fff", fontFamily: "Montserrat" }}>=</h2>
-                <h2 style={{ color: "#fff", fontFamily: "Montserrat" }}>scientific tool</h2> 
-              </div>
-
-              <div className="row">
-                <div className="col-lg gif top-left">
-                  <h3 className="gif-header gif-header-top text-center" style={{ color: "#fff", fontFamily: "Montserrat" }}>Make Your Tool</h3>
-                  <img src="/img/gif1_new.gif" />
-                </div>
-                <div className="col-lg gif top-right">
-                  <h3 className="gif-header gif-header-top text-center" style={{ color: "#fff", fontFamily: "Montserrat" }}>Place on Target</h3>
-                  <img src="/img/gif2_new.gif" />
-                </div>
-              </div>        
-
-              <div className="row">
-                <div className="col-lg gif bottom-left">
-                  <h3 className="gif-header gif-header-top gif-header-mobile text-center" style={{ color: "#fff", fontFamily: "Montserrat" }}>Scan Target</h3>
-                  <img src="/img/gif3_new.gif" />
-                  <h3 className="gif-header gif-header-bottom text-center" style={{ color: "#fff", fontFamily: "Montserrat" }}>Scan Target</h3>
-                </div>
-                <div className="col-lg gif bottom-right">
-                  <h3 className="gif-header gif-header-top gif-header-mobile text-center" style={{ color: "#fff", fontFamily: "Montserrat" }}>Analyze Results</h3>
-                  <img src="/img/gif4_new.gif" />
-                  <h3 className="gif-header gif-header-bottom text-center" style={{ color: "#fff", fontFamily: "Montserrat" }}>Analyze Results</h3>
-                </div>
-              </div>    
-
-            </div>          
-          </div>
-
-          <div className="footer">
-            <div className="container-fluid">
-            <div className="row">
-                <img className="footer-logo" src="/img/look-box-logo.png" />
-              </div>
-              <div className="row">
-                <p className="footer-text text-center">your quantitative sixth sense</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-
-
-        {/* <!-- Modal --> */}
-        <div className="modal fade" id="mc-modal" tabIndex="-1" role="dialog">
-          <div className="modal-dialog modal-dialog-centered" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalCenterTitle">Contact Us</h5>
-                <button type="button" className="close" data-dismiss="modal">
-                  <span>&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <a href="mailto:look.science@gmail.com<" style={{ color: "#fff" }}>look.science@gmail.com</a>
-              </div>
-            </div>
-          </div>
-        </div>
 
       </div>
     );
